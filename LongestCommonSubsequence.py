3d7e7479962ac244e6bc5f0f549bbc91b9589d20
Gaurav Kaushik <vgauravk@gmail.com>
2019-04-21 01:22:22 +0530

    Longest Common Subsequence | DP-4 LCS Problem Statement: Given two sequences, find the length of longest subsequence present in both of them. A subsequence is a sequence that appears in the same relative order, but not necessarily contiguous. For example, “abc”, “abg”, “bdf”, “aeg”, ‘”acefg”, .. etc are subsequences of “abcdefg”. So a string of length n has 2^n different possible subsequences.

--- LongestCommonSubsequence.py
+++ LongestCommonSubsequence.py
@@ -1,9 +1,5 @@
 """
 Longest Common Subsequence | DP-4
-We have discussed Overlapping Subproblems and Optimal Substructure properties in Set 1 and Set 2 respectively.
-We also discussed one example problem in Set 3. Let us discuss Longest Common Subsequence (LCS) problem as one
-more example problem that can be solved using Dynamic Programming.
-
 LCS Problem Statement: Given two sequences, find the length of longest subsequence present in both of them. A
 subsequence is a sequence that appears in the same relative order, but not necessarily contiguous. For example,
 “abc”, “abg”, “bdf”, “aeg”, ‘”acefg”, .. etc are subsequences of “abcdefg”. So a string of length n has 2^n
